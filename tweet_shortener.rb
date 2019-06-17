3bbe59ae5762988c8bb5a5574fa75e4ceef50e5c
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-17 18:17:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,5 @@
+tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
 def dictionary
   substitute = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "and" => "&", "and " => "@" }
 end
@@ -5,13 +7,12 @@ end
 def word_substituter(tweet)
   sentence = tweet.split(" ")
   dic_ar = dictionary.keys
-  sentence.map do |string|
+  sentence.maps do |string|
     i = 0
-    while i < dict_ar.length
+    while i < dic_ar.length
       if string == dic_ar[i]
-        string = dic_ar[i]
-        puts string
-      else i += 1
+        string = dictionary[dic_ar[i]]
+      i += 1
       end
     end
   end
