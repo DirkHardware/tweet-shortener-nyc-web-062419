09b48e6641a0ec81f1be1de450786b104398a3c4
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-17 17:11:03 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,7 @@
 def dictionary
   substitute = {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "and" => "&", "and " => "@" }
 end
+
 def word_substituter(tweet)
 end
 
